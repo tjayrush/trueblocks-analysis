adc886eaa7f62dedec3a7698f03e81fe068b5e96
wildmolasses <changes@gmail.com>
2019-05-14 14:45:32 +0900

    other-data-explore: cosmetic updates

--- other-data-explore/explore.R
+++ other-data-explore/explore.R
@@ -7,11 +7,13 @@ countsByWeek <- read_tsv(paste0(data.dir, "countsByWeek.txt"))
 countsPer10000 <- read_tsv(paste0(data.dir, "countsPer10000.txt"))
 Traces <- readxl::read_xlsx(paste0(data.dir, "Traces.xlsx"), range = "A5:CW255") %>%
   rename(nTraces = `...1`) %>%
-  gather(block.bucket, appearances, -nTraces) %>%
+  gather(block.bucket, occurrences, -nTraces) %>%
   mutate(block.bucket = as.integer(block.bucket))
 
 Traces %>%
-  ggplot(aes(x = block.bucket, y = nTraces, fill = appearances)) +
-  geom_tile(color = "white", size=0.1) + 
-  scale_fill_gradient(name = "n occurrences", trans="pseudo_log") +
+  #mutate(block.bucket = ntile(block.bucket, 25) * 2000000) %>%
+  ggplot(aes(x = block.bucket, y = nTraces, fill = occurrences)) +
+  geom_tile(color = "white", size=0) + 
+  viridis::scale_fill_viridis(name = "log(n occurrences)", trans="pseudo_log", option = "A") +
   theme_minimal(base_size=8)
+
